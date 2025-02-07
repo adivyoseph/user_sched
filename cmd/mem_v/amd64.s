468d8df7d21ee128aa93df8bd11cc66b3df32a28
Adiv <adivyoseph@yahoo.com>
2025-02-07 11:16:36 -0800

    pqos

--- cmd/mem_v/amd64.s
+++ cmd/mem_v/amd64.s
@@ -11,3 +11,13 @@ TEXT ·myCpuid(SB), 7, $0
 	MOVL CX, ecx+16(FP)
 	MOVL DX, edx+20(FP)
 	RET
+
+TEXT ·myCpuidex(SB), 7, $0
+	MOVL op+0(FP), AX
+	MOVL op2+4(FP), CX
+	CPUID
+	MOVL AX, eax+8(FP)
+	MOVL BX, ebx+12(FP)
+	MOVL CX, ecx+16(FP)
+	MOVL DX, edx+20(FP)
+	RET
